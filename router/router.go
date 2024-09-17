a3a797571a5a691e08715d5e6b899dbb199bf013
shbai <1546272904@qq.com>
2024-09-17 16:21:27 +0800

    feat(shbai): 前端登录交互

--- router/router.go
+++ router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"room_rating_guidence/api/controller"
 	"room_rating_guidence/infrs/middleware"
 )
@@ -13,6 +14,10 @@ func InitRouter() *gin.Engine {
 	// 跨域请求中间件
 	router.Use(middleware.Cors())
 	// 图片访问路径静态文件夹可直接访问
+	// 告诉gin框架模板文件引用的静态文件去哪里找
+	router.Static("/static", "static")
+	// 告诉gin框架去哪里找模板文件
+	router.LoadHTMLGlob("templates/*")
 	//router.StaticFS(config.Config.ImageSettings.UploadDir,
 	//	http.Dir(config.Config.ImageSettings.UploadDir))
 	// 日志log中间件
@@ -23,6 +28,13 @@ func InitRouter() *gin.Engine {
 
 // register 注册路由
 func register(router *gin.Engine) {
+
+	router.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.html", nil)
+	})
+	router.GET("/register", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "register.html", nil)
+	})
 
 	// user相关路由
 	user := router.Group("/api/user")
